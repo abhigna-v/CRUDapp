c381cb9ffae09034e39526b1e8f7221e9e4a1b49
abhigna-v <67791070+abhigna-v@users.noreply.github.com>
2021-05-19 09:25:52 +0530

    updated db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose =require ('mongoose');
-var password=process.env.MONGO_atlas_password;
+var password="abhi";//process.env.MONGO_atlas_password;
 var connectionString="mongodb+srv://abhi:"+password+"@cluster0.hhb8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true , useFindAndModify: false});
